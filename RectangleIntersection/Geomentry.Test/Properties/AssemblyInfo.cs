737c971768de92845380cae8b6dc6dbea4cda639
lraj <lraj@dow.com>
2018-06-28 19:07:24 -0400

    Adding Unit Test Project and also further changes to the condition logic

---
+++ RectangleIntersection/Geomentry.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Geomentry.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Geomentry.Test")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b530b97c-812c-402e-8e88-28fc8c11c2fb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RectangleIntersection/Geomentry.Test/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Geometry;
+using Geometry.Shapes;
+
+namespace Geometry.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Extreme_Point_Condition_Not_Satisfies_Should_Return_False()
+        {
+            Rectangle R1 = new Rectangle(3, 2, 6, 4);
+            Rectangle R2 = new Rectangle(8, 2, 11, 4);
+            CheckRectangleIntsection CRI = new CheckRectangleIntsection();
+            Assert.AreEqual(false, CRI.DoesIntersect(R1, R2));
+
+        }
+
+        [TestMethod]
+        public void Extreme_Point_Condition_Satisfies_Should_Return_True()
+        {
+            Rectangle R1 = new Rectangle(3, 2, 6, 4);
+            Rectangle R2 = new Rectangle(5, 4, 8, 6);
+            CheckRectangleIntsection CRI = new CheckRectangleIntsection();
+            Assert.AreEqual(true, CRI.DoesIntersect(R1, R2));
+
+        }
+    }
+}
